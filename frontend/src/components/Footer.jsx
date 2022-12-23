9ca3d1ed686900556ba7c679216f8960955896f3
3rooks <Brookslynx@gmail.com>
2022-12-22 22:01:36 -0300

    ✅ FRONT ENDED

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,19 @@
-const Footer = () => {
-	return (
-		<footer className='flex absolute bottom-0 w-full text-center h-10 justify-center'>
-			<h2>
-				Developed by
-				<b>
-					<a
-						href='https://github.com/3rooks'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						{' '}
-						Brooks
-					</a>
-				</b>
-			</h2>
-		</footer>
-	);
-};
+const Footer = () => (
+	<footer className='flex absolute bottom-0 w-full text-center h-10 justify-center'>
+		<h2>
+			Developed by
+			<b>
+				<a
+					href='https://github.com/3rooks'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					{' '}
+					Brooks
+				</a>
+			</b>
+		</h2>
+	</footer>
+);
 
 export default Footer;
